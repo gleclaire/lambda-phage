db51a791e235f96810975e86b5950634f3f21295
Thomas Hopkins <thopkins@redventures.com>
2015-10-23 14:29:53 -0400

    use prompt struct in init command

--- init.go
+++ init.go
@@ -1,11 +1,9 @@
 package main
 
 import "github.com/hopkinsth/lambda-phage/Godeps/_workspace/src/github.com/spf13/cobra"
-import "github.com/hopkinsth/lambda-phage/Godeps/_workspace/src/github.com/peterh/liner"
 import "github.com/hopkinsth/lambda-phage/Godeps/_workspace/src/github.com/aws/aws-sdk-go/service/iam"
 import "github.com/hopkinsth/lambda-phage/Godeps/_workspace/src/github.com/aws/aws-sdk-go/aws"
 import "github.com/hopkinsth/lambda-phage/Godeps/_workspace/src/github.com/tj/go-debug"
-import "strconv"
 import "strings"
 import "fmt"
 import "os"
@@ -20,73 +18,9 @@ func init() {
 	cmds = append(cmds, initCmd)
 }
 
-type prompt struct {
-	text           string
-	description    string
-	def            string
-	required       bool
-	stringStore    **string
-	stringSetStore *[]*string
-	intStore       **int64
-	funcStore      func(string)
-	completer      func(string) []string
-}
-
-func newPrompt() *prompt {
-	return new(prompt)
-}
-
-func (p *prompt) withCompleter(f liner.Completer) *prompt {
-	p.completer = f
-	return p
-}
-
-func (p *prompt) isRequired() *prompt {
-	p.required = true
-	return p
-}
-
-func (p *prompt) setDef(d string) *prompt {
-	p.def = d
-	return p
-}
-
-func (p *prompt) setText(t string) *prompt {
-	p.text = t
-	return p
-}
-
-func (p *prompt) setDescription(t string) *prompt {
-	p.description = t
-	return p
-}
-
-func (p *prompt) withString(s **string) *prompt {
-	p.stringStore = s
-	return p
-}
-
-func (p *prompt) withStringSet(s *[]*string) *prompt {
-	p.stringSetStore = s
-	return p
-}
-
-func (p *prompt) withInt(s **int64) *prompt {
-	p.intStore = s
-	return p
-}
-
-func (p *prompt) withFunc(s func(string)) *prompt {
-	p.funcStore = s
-	return p
-}
-
 // helps you build a config file
 func initPhage(c *cobra.Command, _ []string) {
 	var err error
-	l := liner.NewLiner()
-	defer l.Close()
-	l.SetCtrlCAborts(true)
 	fmt.Println(`
   HELLO AND WELCOME
   
@@ -98,94 +32,12 @@ func initPhage(c *cobra.Command, _ []string) {
   Please answer the prompts as they appear below:
 	`)
 
-	//reqMsg := "Sorry, that field is required. Try again."
-
-	// set this callback we can use to call all the stuff
-	var realCompleter liner.Completer
-	l.SetCompleter(func(line string) []string {
-		if realCompleter != nil {
-			return realCompleter(line)
-		}
-		return nil
-	})
-
 	iCfg := new(Config)
 	iCfg.IamRole = new(IamRole)
 	iCfg.Location = new(Location)
 	prompts := getPrompts(iCfg)
 
-	for _, cPrompt := range prompts {
-		p := cPrompt
-		text := p.text
-		if p.def != "" {
-			text += " [" + p.def + "]"
-		}
-
-		text += ": "
-
-		realCompleter = nil
-		if p.completer != nil {
-			realCompleter = p.completer
-		}
-
-		if p.description != "" {
-			fmt.Print(p.description)
-		}
-
-		if s, err := l.Prompt(text); err == nil {
-			input := s
-			hasInput := input != ""
-			if p.stringStore != nil {
-				if hasInput {
-					*p.stringStore = &input
-				} else {
-					*p.stringStore = &p.def
-				}
-
-			} else if p.stringSetStore != nil {
-				var splitMe string
-				if hasInput {
-					splitMe = input
-				} else {
-					splitMe = p.def
-				}
-
-				spl := strings.Split(splitMe, ",")
-				pspl := make([]*string, len(spl))
-				for i, v := range spl {
-					// we need to set the value
-					// in a variable local to this block
-					// because the pointed-to value in
-					// `v` will change on the next
-					// loop iteration
-					realVal := v
-					pspl[i] = &realVal
-				}
-
-				*p.stringSetStore = pspl
-
-			} else if p.intStore != nil {
-				var tParse string
-				if hasInput {
-					tParse = input
-				} else {
-					tParse = p.def
-				}
-
-				i, _ := strconv.ParseInt(tParse, 10, 64)
-				*p.intStore = &i
-			} else if p.funcStore != nil {
-				// just call the function, man
-				p.funcStore(s)
-			}
-		} else if err == liner.ErrPromptAborted {
-			fmt.Println("Aborted")
-			return
-		} else {
-			fmt.Println("Error reading line: ", err)
-			return
-		}
-	}
+	err = prompts.run()
 
 	// merge in any existing properties from the config object
 	var wCfg *Config
@@ -206,119 +58,130 @@ func initPhage(c *cobra.Command, _ []string) {
 }
 
 // returns all the prompts needed for the `init` command
-func getPrompts(cfg *Config) []*prompt {
+func getPrompts(cfg *Config) prompter {
 	wd, _ := os.Getwd()
 	st, _ := os.Stat(wd)
 
 	iamRoles, roleMap := getIamRoles()
+	ps := new(promptSet)
 
-	return []*prompt{
-		newPrompt().
-			withString(&cfg.Name).
-			isRequired().
-			setText("Enter a project name").
-			setDef(st.Name()),
-		newPrompt().
-			withString(&cfg.Description).
-			setText("Enter a project description if you'd like").
-			setDef(""),
-		newPrompt().
-			withString(&cfg.Archive).
-			setText("Enter a archive name if you'd like").
-			setDef(st.Name() + ".zip"),
-		newPrompt().
-			withString(&cfg.Runtime).
-			isRequired().
-			setText("What runtime are you using: nodejs, java8, or python 2.7?").
-			setDef("nodejs").
-			withCompleter(
-			func(l string) []string {
-				// there can only be one
-				r := make([]string, 1)
-				if len(l) == 0 {
-					r[0] = ""
+	return ps.add().
+		withString(&cfg.Name).
+		isRequired().
+		setText("Enter a project name").
+		setDef(st.Name()).
+		done().
+		add().
+		withString(&cfg.Description).
+		setText("Enter a project description if you'd like").
+		setDef("").
+		done().
+		add().
+		withString(&cfg.Archive).
+		setText("Enter a archive name if you'd like").
+		setDef(st.Name() + ".zip").
+		done().
+		add().
+		withString(&cfg.Runtime).
+		isRequired().
+		setText("What runtime are you using: nodejs, java8, or python 2.7?").
+		setDef("nodejs").
+		withCompleter(
+		func(l string) []string {
+			// there can only be one
+			r := make([]string, 1)
+			if len(l) == 0 {
+				r[0] = ""
+			} else {
+				switch string(l[0]) {
+				case "n":
+					r[0] = "nodejs"
+				case "j":
+					r[0] = "java8"
+				case "p":
+					r[0] = "python2.7"
+				}
+			}
+
+			return r
+		},
+	).
+		done().
+		add().
+		withString(&cfg.EntryPoint).
+		isRequired().
+		setText("Enter an entry point or handler name").
+		setDef("index.handler").
+		done().
+		add().
+		withInt(&cfg.MemorySize).
+		setText("Enter memory size").
+		setDef("128").
+		done().
+		add().
+		withInt(&cfg.Timeout).
+		setText("Enter timeout").
+		setDef("5").
+		done().
+		add().
+		withStringSet(&cfg.Regions).
+		setText("Enter AWS regions where this function will run").
+		setDef("us-east-1").
+		done().
+		add().
+		withFunc(
+		func(s string) {
+			// if this looks like an ARN,
+			// we'll assume it is... for now
+			if strings.Index(s, "arn:aws:iam::") == 0 {
+				cfg.IamRole.Arn = &s
+			} else {
+				// check to see if this name is inside the role map
+				if arn, ok := roleMap[s]; ok {
+					cfg.IamRole.Arn = arn
 				} else {
-					switch string(l[0]) {
-					case "n":
-						r[0] = "nodejs"
-					case "j":
-						r[0] = "java8"
-					case "p":
-						r[0] = "python2.7"
-					}
+					// if not in role map, set the name
+					cfg.IamRole.Name = &s
 				}
+			}
+		},
+	).
+		isRequired().
+		setText("IAM Role").
+		setDescription(
+		"\nAll Lambda functions must run with an IAM role, which gives them access\n" +
+			"to various resources in AWS. What role do you want to assign this function?\n" +
+			"Type a name and we'll try to auto-complete it if you press the tab key\n",
+	).
+		setDef("").
+		withCompleter(
+		func(l string) []string {
+			c := make([]string, 0)
+			for _, role := range iamRoles {
+				if strings.HasPrefix(*role.Name, l) {
+					c = append(c, *role.Name)
+				}
+			}
 
-				return r
-			},
-		),
-		newPrompt().
-			withString(&cfg.EntryPoint).
-			isRequired().
-			setText("Enter an entry point or handler name").
-			setDef("index.handler"),
-		newPrompt().
-			withInt(&cfg.MemorySize).
-			setText("Enter memory size").
-			setDef("128"),
-		newPrompt().
-			withInt(&cfg.Timeout).
-			setText("Enter timeout").
-			setDef("5"),
-		newPrompt().
-			withStringSet(&cfg.Regions).
-			setText("Enter AWS regions where this function will run").
-			setDef("us-east-1"),
-		newPrompt().
-			withFunc(
-			func(s string) {
-				// if this looks like an ARN,
-				// we'll assume it is... for now
-				if strings.Index(s, "arn:aws:iam::") == 0 {
-					cfg.IamRole.Arn = &s
-				} else {
-					// check to see if this name is inside the role map
-					if arn, ok := roleMap[s]; ok {
-						cfg.IamRole.Arn = arn
-					} else {
-						// if not in role map, set the name
-						cfg.IamRole.Name = &s
-					}
-				}
-			},
-		).
-			isRequired().
-			setText("IAM Role").
-			setDescription(
-			"\nAll Lambda functions must run with an IAM role, which gives them access\n" +
-				"to various resources in AWS. What role do you want to assign this function?\n" +
-				"Type a name and we'll try to auto-complete it if you press the tab key\n",
-		).
-			setDef("").
-			withCompleter(
-			func(l string) []string {
-				c := make([]string, 0)
-				for _, role := range iamRoles {
-					if strings.HasPrefix(*role.Name, l) {
-						c = append(c, *role.Name)
-					}
-				}
-
-				return c
-			},
-		),
-		newPrompt().
-			withString(&cfg.Location.S3Bucket).
-			setText("s3 bucket you want to upload your code to (if any)").
-			setDef(""),
-		newPrompt().
-			withString(&cfg.Location.S3Key).
-			setText("s3 folder your code should be stored inside (if any)").
-			setDef(""),
-		newPrompt().
-			withString(&cfg.Location.S3Region).
-			setText("s3 region where your bucket is (if any)").
-			setDef(""),
-	}
+			return c
+		},
+	).
+		done().
+		add().
+		withString(&cfg.Location.S3Bucket).
+		setText("s3 bucket you want to upload your code to (if any)").
+		setDef("").
+		done().
+		add().
+		withString(&cfg.Location.S3Key).
+		setText("s3 folder your code should be stored inside (if any)").
+		setDef("").
+		done().
+		add().
+		withString(&cfg.Location.S3Region).
+		setText("s3 region where your bucket is (if any)").
+		setDef("").
+		done()
 }
 
 // pulls all the IAM roles from your account
